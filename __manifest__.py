217f26bd48fe27236e85224c24c7b7e0754c8358
WK <wisaruthk@gmail.com>
2018-04-10 16:48:01 +0700

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Report PDF Customize',
+    'description': 'Customize report.',
+    'author': 'Wisaruth K.',
+    'depends': ['web'],
+    'data': [
+        # QWeb Report
+        'reports/report_custom.xml'
+    ]
+}
